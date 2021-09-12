1c03b286d266490525d6a03c31f6397e65505d69
Farid <mdfaridulislam509@gmail.com>
2021-09-12 15:59:51 +0800

    my third commit

--- first.js
+++ first.js
@@ -61,7 +61,7 @@ gradingSystem('bangladesh')
 
 
 //3. Use of normal function
-
+ /***************************************************
  function addNumber(num1, num2){
    
    const sum = num1 + num2;
@@ -88,11 +88,119 @@ const numbers = (num)=>{
    }
 }
 numbers(10);
- 
 
 
+const numbers = (num)=>{
+
+   for ( let i = 1; i <num; i++){
+      console.log(i);
+   }
+}
+numbers(10);
 
 // print all of the even number in 1 - 100 (function and loop)
+function evenNumber(number) {
+   for(let i = 1; i<=number; i++){
+      if( i % 2 === 0){
+         console.log(i)
+      }
+   }
+}
+evenNumber(100)
+
+
+// print all of the number in 1 - 100 which are divided by 3(function and loop)
+
+const oddNumbers = (number) =>{
+   for( i = 1; i < number ; i++){
+      if(i % 2 === 1){
+         console.log(i)
+      }
+   }
+} 
+oddNumbers(100);
+ ******************************************************/ 
+
+
+
+/* *****************************************************
+// Uses of loop (for, while, do while)
+
+// for loop 
+const friendName = ['farid', 'abir', 'rony'];
+for(let i=0; i<1; i++){
+   console.log(friendName);
+} 
+
+// while loop
+let number = 1;
+while(number < 10){
+   console.log(number);
+   number++
+
+   // do while loop
+let i = 1;
+do{
+      if(i%2 === 0){
+         console.log(i);
+      }  
+      i++ ;
+}
+while(i<20)
+
+
+// for of loop (specially use for array)
+const numbers = [10, 20, 50, 85, 30];
+   
+   for (const number of numbers) {
+      console.log(number)  
+   }
+
+// for in loop (this loop is specially use for object)
+function myPhones (){
+   const numbers = [10, 20, 50, 85, 30];
+   const phones = {
+   
+      Brand:'samsung', color:'red', quality: 'Good', Quantity:20,
+      Brand: 'Sony', color:'Yellow', Quality: 'Vary-good', Quantity: 82,
+      Brand: 'Nokia', color:'Green', Quality: 'Also-good', Quantity: 58
+   
+   }
+   for (const number in numbers) {
+      console.log(number)  
+   }
+}
+myPhones()
+}
+****************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
